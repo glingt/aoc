e9f153f7723818dc919d7bc222daf5cec89f3115
glingt <nilser@gmail.com>
2019-11-09 12:53:19 +0100

    add prettier and eslint

--- src/dec1/index.ts
+++ src/dec1/index.ts
@@ -2,9 +2,23 @@
 import * as fs from "fs";
 
 const content = fs.readFileSync("./src/dec1/input.txt");
-const lines = content.toString().split('\r\n');
-console.log(lines[lines.length- 1]);
+const terms = content.toString().split('\r\n').map(line => parseInt(line, 10));
 
-const n = lines.reduce((sum, line) => sum + parseInt(line, 10), 0);
+const n = terms.reduce((sum, term) => sum + term, 0);
 
 console.log(n);
+
+let cuml = 0;
+const cumls: number[] = [0];
+
+for (let i = 0; i < terms.length; i++) {
+  cuml += terms[i];
+  if (cumls.indexOf(cuml) !== -1) {
+    console.log(cuml);
+    break;
+  } else {
+  }
+  cumls.push(cuml);
+}
+
+console.log(cumls.slice(0, 10));
